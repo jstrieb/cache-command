17ac558979b373e5877107868a2778824a5fbe45
Jacob Strieb <jstrieb@users.noreply.github.com>
2022-05-01 13:14:37 -0400

    Fix to pass shellcheck

--- cache.sh
+++ cache.sh
@@ -15,9 +15,9 @@ ARG_HASH="$(echo "$@" \
   | grep --only-matching --extended-regexp '\w+')"
 FILE_PATH="${TMP_DIR}/${ARG_HASH}"
 if [ ! -f "${FILE_PATH}" ]; then
-  echo "Running: $@" 2>&1
+  echo "Running: $*" 2>&1
   eval "$@" > "${FILE_PATH}"
 else
-  echo "Loading command from cache: $@" 2>&1
+  echo "Loading command from cache: $*" 2>&1
 fi
 cat "${FILE_PATH}"
